5292659dbe44488cb0324b3c3397b77da07eb2ed
flexdevru <109109928+flexdevru@users.noreply.github.com>
2022-07-20 10:00:34 +0200

    Initial commit

---
+++ src/managers/StorylineManager.d.ts
@@ -0,0 +1,16 @@
+export declare class StorylineManager {
+    private static _instance;
+    private storyLine;
+    private player;
+    static get instance(): StorylineManager;
+    constructor();
+    get inPlayer(): boolean;
+    goNext: () => void;
+    goNextSlide: () => void;
+    setVar: (variable: string, value?: any) => void;
+    getVar: (variable: string) => string;
+    getTaskVar: (variable: string) => string;
+    getTaskShowHelpVar: () => string;
+    getTaskCompletedVar: () => string;
+    getTaskStoreVar: () => string;
+}
